6b9d481dc8fa08b5f04d48cdec67aad7b1c7de95
UmeshPandit264 <coderprajapati264@gmail.com>
2025-03-19 08:55:15 +0530

    first commit

---
+++ myapp12/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NoPage from './pages/NoPage'
+import Layout from './pages/Layout'
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="register" element={<Register />}></Route>
+            <Route path="*" element={<NoPage />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
